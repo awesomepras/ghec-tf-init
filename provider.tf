e77c1a3cf5458650e5d72b54d07bbbf1dda4d907
awesomepras <prasana@alienfromopondy.com>
2025-02-27 22:15:22 -0500

    fixed providers

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
-
-  required_version = ">= 1.0"
-}
-
 provider "github" {
   token = var.github_token
   owner = var.org_name
